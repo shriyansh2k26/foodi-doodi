63fbc70ec93a0001745503c02b57edbebd0a67fa
Shriyansh Singh <140579653+shriyansh2k26@users.noreply.github.com>
2023-11-29 22:31:43 +0530

    Add files via upload

---
+++ src/components/AdminPage/Admin.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import Navbar from '../Navbar/Navbar'
+import './admin.css'
+import '../../utils.css'
+export default function Admin() {
+  const [category, setCategory] = useState("");
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
+  const [description, setDes] = useState("")
+  const [price, setPrice] = useState("")
+  const [id, setId] = useState("")
+  const handleAddMenu = async (e) => {
+    e.preventDefault();
+    const response = await fetch('https://foodservers.onrender.com/addmenu', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image, title, description, price, id, category
+      })
+    })
+    const data = await response.json();
+    setImage("")
+    setPrice("")
+    setTitle("")
+    setDes("")
+    setId("")
+    setCategory("")
+  }
+  const [mode, setMode] = useState('add');
+  const switchMode = (newMode) => {
+    setMode(newMode);
+  };
+  const EditComponent=()=>{
+    return<>
+    <h1>Edit</h1>
+    </>
+  }
+
+  const AddComponent = () => {
+    return (<>
+    
+    </>)
+  }
+  return (
+    <div>
+      <Navbar />
+      <div className=" flex jus-center">
+      <button onClick={() => switchMode('add')}>Add </button>
+      <button onClick={() => switchMode('edit')}>Edit </button>
+      </div>
+      {mode === 'add' ?<>
+      <div class="menu-item">
+        <label for="item-name">Id :</label>
+        <input type="text" onChange={(e) => { setId(e.target.value) }} value={id} id="item-name" />
+      </div>
+      <div class="menu-item">
+        <label for="item-name">Item Name:</label>
+        <input type="text" onChange={(e) => { setTitle(e.target.value) }} value={title} id="item-name" />
+      </div>
+      <div class="menu-item">
+        <label for="item-img">Image Url:</label>
+        <input type="text" onChange={(e) => { setImage(e.target.value) }} value={image} id="item-img" />
+      </div>
+      <div class="menu-item">
+        <label for="itemdes">Category:</label>
+        <input type="text" onChange={(e) => { setCategory(e.target.value) }} value={category} id="item-des" />
+      </div>
+      <div class="menu-item">
+        <label for="itemdes">Description:</label>
+        <input type="text" onChange={(e) => { setDes(e.target.value) }} value={description} id="item-des" />
+      </div>
+      <div class="menu-item">
+        <label for="item-price">Item Price:</label>
+        <input type="text" onChange={(e) => { setPrice(e.target.value) }} value={price} id="item-price" />
+      </div>
+
+      <div class="menu-item">
+        <button onClick={handleAddMenu}>Add Item</button>
+      </div></>: <EditComponent />}
+    </div>
+
+
+  )
+}
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useState } from 'react'
+import './login.css'
+import {Link, useNavigate} from 'react-router-dom'
+import {AuthContext} from '../../context/authContext';
+import { toast } from 'react-toastify'; 
+export default function Login() {
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const navigate=useNavigate();
+  const {auth,setAuth}=useContext(AuthContext);
+ const handleLogin=async(e)=>{
+  e.preventDefault();
+  const response=await fetch("https://foodservers.onrender.com/auth/login",{
+    method:"POST",
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+        email,password
+    })
+  })
+  const data=await response.json();
+  if(data.success){
+    toast.success('Logged In Successfully', {
+      position: "top-center",
+      autoClose: 1200,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+    const navigateToHome=()=>{
+        navigate('/')
+    }
+    navigateToHome();
+    setAuth({
+      ...auth,
+      token:data.token,
+      email:data.email
+    })
+    localStorage.setItem('auth',JSON.stringify(data))
+  }
+  else{
+    toast.error('Invalid Credentials', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      }); 
+  }
+ }
+  return (
+    <div class="container-lgn">
+    <h2>Login to Food App</h2>
+    <form onSubmit={handleLogin}>
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
+        </div>
+        <input type="submit" value="Login"/>
+        <span><Link to='/auth/register'>New User? Register</Link></span>
+    </form>
+</div>
+  )
+}
